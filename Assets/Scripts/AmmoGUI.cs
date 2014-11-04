5b57adc602651cebfa428fc5ab16311801a3b702
Rasmus Larsen <doctorherbstein@gmail.com>
2014-11-04 16:54:49 +0100

    No change

--- Assets/Scripts/AmmoGUI.cs
+++ Assets/Scripts/AmmoGUI.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 
 [ExecuteInEditMode]
-public class AmmoGUI : MonoBehaviour {
+public class AmmoGUI : MonoBehaviour
+{
 
     private ShootingScript spawnPoint;
     public GUIStyle ammoCount;
- 
-    void Awake()
+
+    private void Awake()
     {
         spawnPoint = GameObject.FindGameObjectWithTag("Player").GetComponent<ShootingScript>();
     }
-    
-    void OnGUI()
+
+    private void OnGUI()
     {
         GUI.Label(new Rect(0, 0, 0, 0), "" + spawnPoint.bulletGun.Ammo + "  " + spawnPoint.lightGun.Ammo, ammoCount);
     }
