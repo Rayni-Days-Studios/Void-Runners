a12cb32a03f64c67b4bc7f50ffa66bc33ee0c206
Rasmus Larsen <doctorherbstein@gmail.com>
2014-11-04 09:20:05 +0100

    Added mouse lock support

--- Assets/Scripts/Utility/LockMouse.cs
+++ Assets/Scripts/Utility/LockMouse.cs
@@ -2,15 +2,22 @@
 
 public class LockMouse : MonoBehaviour {
 
+    void Awake()
+    {
+        Screen.lockCursor = true;
+    }
+
 	// Use this for initialization
 	void Start ()
     {
-	
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	    {
+	        Screen.lockCursor = !Screen.lockCursor;
+	    }
 	}
 }
