aa1e1eeba3e6c07ae162be08fe42b13bdb8ac79c
Rasmus Larsen <doctorherbstein@gmail.com>
2014-11-08 15:03:14 +0100

    Added player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform BarrelEnd;
+    public bool UsePrimaryGun; // True = LightGun, False = BulletGun
+    public Gun LightGun;
+    public Gun BulletGun;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //If left click
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (UsePrimaryGun && LightGun.LoadedAmmo > 0)
+                LightGun.Shoot(BarrelEnd);
+            else
+                print("No more energy left, reload!");
+
+            if (!UsePrimaryGun && BulletGun.LoadedAmmo > 0)
+                BulletGun.Shoot(BarrelEnd);
+            else
+                print("No more bullets, reload!");
+        }
+
+        //If pressing R
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (UsePrimaryGun)
+                LightGun.Reload();
+            else
+                BulletGun.Reload();
+        }
+	}
+}
