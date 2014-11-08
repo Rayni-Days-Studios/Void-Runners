e5a27d93bbd2de331adfe544147cd187c0269657
Rasmus Larsen <doctorherbstein@gmail.com>
2014-11-08 14:58:24 +0100

    Moving Gun definition to own file

---
+++ Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Gun : MonoBehaviour {
+    public int TotalAmmo;
+    public int LoadedAmmo;             //Amount of bullets not currently in gun
+    public int AmmoCap;                 //Ammo capacity
+    public float Force;
+    public Rigidbody BulletPrefab;
+    public GameObject Bullet;
+    public AudioSource ShootSound;
+
+    public void Shoot(Transform spawnPoint)
+    {
+        LoadedAmmo -= 1;
+        ShootSound.Play();
+        //Shoot
+        Rigidbody bulletInstance = Instantiate(BulletPrefab, spawnPoint.position, spawnPoint.rotation) as Rigidbody;
+
+        if (bulletInstance != null)
+            bulletInstance.AddForce(spawnPoint.forward * Time.deltaTime * Force * 1000f);
+    }
+
+    public void Reload()
+    {
+        // If there is more outside the gun than missing inside.
+        if (TotalAmmo > AmmoCap - LoadedAmmo)
+        {
+            TotalAmmo -= AmmoCap - LoadedAmmo;
+            LoadedAmmo += AmmoCap - LoadedAmmo;
+        }
+        // There's still ammo outside the gun, but not enough to fill it up
+        else if (TotalAmmo > 1 && TotalAmmo < AmmoCap - LoadedAmmo)
+        {
+            // We know that AmmoClip can't fill Ammo, so we don't do a check here.
+            LoadedAmmo += TotalAmmo;
+            TotalAmmo = 0;
+        }
+        else if (TotalAmmo < 1)
+        {
+            // Theres no more clips left, so we can't reload
+            Debug.Log("Out of ammo");
+        }
+    }
+}
